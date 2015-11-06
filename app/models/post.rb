14b557dc08b8307374468af4bda01f8b19ad97f8
Daniel Duarte <danielduartego@gmail.com>
2015-11-05 17:15:13 -0800

    start to create a conection post to user, one to many

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,6 @@ class Post < ActiveRecord::Base
 
   has_many :comments, dependent: :destroy
 
+  belongs_to :user
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105015720) do
+ActiveRecord::Schema.define(version: 20151106011327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,9 +46,11 @@ ActiveRecord::Schema.define(version: 20151105015720) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "comment_id"
+    t.integer  "user_id"
   end
 
   add_index "posts", ["comment_id"], name: "index_posts_on_comment_id", using: :btree
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -63,4 +65,5 @@ ActiveRecord::Schema.define(version: 20151105015720) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "posts", "comments"
+  add_foreign_key "posts", "users"
 end
