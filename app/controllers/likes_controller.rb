8023c129a95cca8f792b688989ccb553f35b4752
Daniel Duarte <danielduartego@gmail.com>
2015-11-24 23:34:35 -0800

    ajaxify like button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,23 +3,31 @@ class LikesController < ApplicationController
   before_action :authenticate_user
 
   def create
-    like = Like.new
-    post = Post.find params[:post_id]
-    like.post = post
-    like.user = current_user
-    if like.save
-      LikesMailer.notify_post_owner(like).deliver_now
-      redirect_to post_path(post)
-    else
-      redirect_to post_path(post), alert: "you can't like"
+    @like = Like.new
+    @post = Post.find params[:post_id]
+    @like.post = @post
+    @like.user = current_user
+    respond_to do |format|
+      if current_user != @post.user
+        @like.save
+        # LikesMailer.notify_post_owner(like).deliver_now
+        format.html {redirect_to post_path(post)}
+        format.js {render :create_success}
+      else
+        format.html {redirect_to post_path(post), alert: "you can't like"}
+        format.js {render js: "alert('can not like');"}
+      end
     end
   end
 
   def destroy
-    post = Post.find params[:post_id]
-    like = current_user.likes.find params[:id]
-    like.destroy
-    redirect_to post_path(post)
+    @post = Post.find params[:post_id]
+    @like = current_user.likes.find params[:id]
+    @like.destroy
+      respond_to do |format|
+        format.html {redirect_to post_path(post)}
+        format.js { render }
+      end
   end
 
 
